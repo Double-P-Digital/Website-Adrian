8ec9e26b0f0cb45c04df177e28505593a5d05195
alebercu <149473284+alebercu@users.noreply.github.com>
2025-09-21 15:18:07 +0300

    deleted more stuff

--- chisfis-nextjs/src/app/(app)/(categories)/(stay)/stay-categories/layout.tsx
+++ chisfis-nextjs/src/app/(app)/(categories)/(stay)/stay-categories/layout.tsx
@@ -1,12 +1,8 @@
 import { ApplicationLayout } from '@/app/(app)/application-layout'
-import BackgroundSection from '@/components/BackgroundSection'
 import BgGlassmorphism from '@/components/BgGlassmorphism'
-import SectionGridAuthorBox from '@/components/SectionGridAuthorBox'
-import SectionSliderNewCategories from '@/components/SectionSliderNewCategories'
-import SectionSubscribe2 from '@/components/SectionSubscribe2'
+
 import { getAuthors } from '@/data/authors'
 import { getStayCategories } from '@/data/categories'
-import Heading from '@/shared/Heading'
 import { ReactNode } from 'react'
 
 const Layout = async ({ children }: { children: ReactNode }) => {
@@ -19,7 +15,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
 
       {children}
 
-      <div className="container">
+      {/* <div className="container">
         <div className="relative py-16 lg:py-20">
           <BackgroundSection />
           <Heading subheading="Explore houses based on 10 types of stays">Explore the world with us.</Heading>
@@ -29,15 +25,7 @@ const Layout = async ({ children }: { children: ReactNode }) => {
             categoryCardType="card5"
           />
         </div>
-        <SectionSubscribe2 className="py-24 lg:py-32" />
-        <div className="relative mb-24 py-16 lg:mb-28 lg:py-20">
-          <BackgroundSection />
-          <Heading subheading="Meet our top 10 authors of the month" isCenter>
-            Top 10 author of the month.
-          </Heading>
-          <SectionGridAuthorBox authors={authors} />
-        </div>
-      </div>
+      </div> */}
     </ApplicationLayout>
   )
 }
