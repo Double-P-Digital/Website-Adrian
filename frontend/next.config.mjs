aaa5888a4d686d24fee332d43bb7d6b2ae4fedd5
parnau-patrick <parnau.patrick@yahoo.com>
2025-12-09 12:46:41 +0200

    Make the images clear and not process with the next server

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,52 +2,9 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: false,
+
   images: {
-    minimumCacheTTL: 2678400 * 6,
-    // Next.js only supports AVIF and WebP formats
-    formats: ['image/avif', 'image/webp'], 
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], 
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384], 
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '', 
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'a0.muscache.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-    // Allow unoptimized images for invalid URLs (fallback)
-    unoptimized: false,
+    unoptimized: true, 
   },
 }
 
